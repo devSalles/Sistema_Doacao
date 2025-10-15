7d1fda2b5a93e15056ba3be25316a41d9a6a56e6
Bernardo de Salles Cruz <168240480+devSalles@users.noreply.github.com>
2025-10-15 14:12:00 -0300

    Upload projeto

---
+++ Sistema_de_Doacoes/src/main/java/Sistema_de_Doacoes/SistemaDeDoacoesApplication.java
@@ -0,0 +1,13 @@
+package Sistema_de_Doacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeDoacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeDoacoesApplication.class, args);
+	}
+
+}
---
+++ Sistema_de_Doacoes/src/test/java/Sistema_de_Doacoes/SistemaDeDoacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package Sistema_de_Doacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeDoacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
